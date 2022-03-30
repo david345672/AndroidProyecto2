19cfc249b4fa5cd1043870436f06daaea1efb4e0
david345672 <davidmur12@gmail.com>
2022-03-30 11:03:20 +0200

    update

--- app/src/main/java/com/example/androidproyecto2/Fragments/MenuListasSkillsFragment/SkillsListsSkillAdpterViewPager.java
+++ app/src/main/java/com/example/androidproyecto2/Fragments/MenuListasSkillsFragment/SkillsListsSkillAdpterViewPager.java
@@ -7,10 +7,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.PagerAdapter;
 
 import com.example.androidproyecto2.Clases.LlistaSkills;
 import com.example.androidproyecto2.Clases.Skill;
+import com.example.androidproyecto2.Fragments.FragmentsAlumno.ValoracionAlumnoFragment.ValoracionAlumnoFragment;
+import com.example.androidproyecto2.Fragments.FragmentsDocente.ValoracionDocenteFragment.ValoracionDocenteFragment;
+import com.example.androidproyecto2.MainActivity;
 import com.example.androidproyecto2.R;
 
 import java.util.List;
@@ -19,6 +24,8 @@ public class SkillsListsSkillAdpterViewPager extends PagerAdapter
 {
     Context context;
     List<Skill> skills;
+    FragmentManager mg;
+    FragmentTransaction fragmentTransaction;
 
     public SkillsListsSkillAdpterViewPager(Context context, List<Skill> skills) {
         this.context = context;
@@ -35,6 +42,16 @@ public class SkillsListsSkillAdpterViewPager extends PagerAdapter
         btnSkill.setText(S.getNom());
         btnSkill.setBackgroundColor(S.getColorFondo());
         btnSkill.setTextColor(S.getColorTexto());
+
+        btnSkill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                irAValoracionProfesor();
+            }
+        });
+
+
+
         container.addView(view);
 
 
@@ -59,6 +76,31 @@ public class SkillsListsSkillAdpterViewPager extends PagerAdapter
     public void destroyItem(ViewGroup collection, int position, Object view) {
         collection.removeView((View) view);
     }
+
+    private void irAValoracionProfesor()
+    {
+
+        MainActivity activity = (MainActivity) context;
+        mg = activity.getSupportFragmentManager();
+        fragmentTransaction = mg.beginTransaction();
+        ValoracionDocenteFragment valoracionDocenteFragment = new ValoracionDocenteFragment();
+        fragmentTransaction.replace(R.id.FrContent,valoracionDocenteFragment);
+        fragmentTransaction.commit();
+
+    }
+
+
+    private void irAValoracionAlumno()
+    {
+        MainActivity activity = (MainActivity) context;
+        mg = activity.getSupportFragmentManager();
+        fragmentTransaction = mg.beginTransaction();
+        ValoracionAlumnoFragment valoracionDocenteFragment = new ValoracionAlumnoFragment();
+        fragmentTransaction.replace(R.id.FrContent,valoracionDocenteFragment);
+        fragmentTransaction.commit();
+    }
+
+
 
 
 }
