51fb24966877264e98e913803bfaf358538b7a68
david345672 <davidmur12@gmail.com>
2022-04-01 13:19:50 +0200

    update

---
+++ app/src/main/java/com/example/androidproyecto2/Fragments/FragmentsDocente/ValoracionDocenteFragment/SkillsValoracionAdapterViewPager.java
@@ -0,0 +1,104 @@
+package com.example.androidproyecto2.Fragments.FragmentsDocente.ValoracionDocenteFragment;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.androidproyecto2.Clases.LlistaSkills;
+import com.example.androidproyecto2.Clases.Skill;
+import com.example.androidproyecto2.MainActivity;
+import com.example.androidproyecto2.R;
+
+import java.util.ConcurrentModificationException;
+import java.util.List;
+
+public class SkillsValoracionAdapterViewPager extends PagerAdapter
+{
+    private MainActivity activity;
+    private Context context;
+    private List<Skill> skills;
+
+
+    public SkillsValoracionAdapterViewPager(Context context, List<Skill> skills, MainActivity activity) {
+        this.context = context;
+        this.skills = skills;
+        this.activity = activity;
+    }
+
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position)
+    {
+        View view = LayoutInflater.from(context).inflate(R.layout.skill_valoracion_docente_viewpager_item,container,false);
+
+        TableLayout tableSkill = view.findViewById(R.id.tableSkill);
+
+        for (int i = 0; i < skills.get(position).getKpis().size(); i++)
+        {
+            TableRow row = new TableRow(context);
+            TextView SubSkill = new TextView(context);
+            SubSkill.setText(skills.get(position).getKpis().get(i).getNom());
+            row.addView(SubSkill);
+
+//            RadioGroup radioGroup = new RadioGroup(context);
+//            RadioButton rdb1 = new RadioButton(context);
+//            RadioButton rdb2 = new RadioButton(context);
+//            RadioButton rdb3 = new RadioButton(context);
+//            RadioButton rdb4 = new RadioButton(context);
+//            radioGroup.addView(rdb1);
+//            radioGroup.addView(rdb2);
+//            radioGroup.addView(rdb3);
+//            radioGroup.addView(rdb4);
+//
+//            row.addView(rdb1);
+//            row.addView(rdb2);
+//            row.addView(rdb3);
+//            row.addView(rdb4);
+
+            tableSkill.addView(row);
+
+        }
+
+
+
+        container.addView(view);
+        return view;
+    }
+
+
+
+
+
+
+
+    @Override
+    public int getCount() {
+        return skills.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup collection, int position, Object view) {
+        collection.removeView((View) view);
+    }
+
+
+}
--- app/src/main/java/com/example/androidproyecto2/Fragments/FragmentsDocente/ValoracionDocenteFragment/ValoracionDocenteFragment.java
+++ app/src/main/java/com/example/androidproyecto2/Fragments/FragmentsDocente/ValoracionDocenteFragment/ValoracionDocenteFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 
 import com.example.androidproyecto2.Clases.LlistaSkills;
 import com.example.androidproyecto2.Clases.Skill;
+import com.example.androidproyecto2.Fragments.MenuListasSkillsFragment.LlistasSkillsGrupAdapterViewPager;
 import com.example.androidproyecto2.MainActivity;
 import com.example.androidproyecto2.R;
 
@@ -24,6 +26,7 @@ public class ValoracionDocenteFragment extends Fragment {
 
     MainActivity activity;
     private List<Skill> skills = new ArrayList<>();
+    ViewPager vpSkillsAValorar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,7 +44,9 @@ public class ValoracionDocenteFragment extends Fragment {
         activity = (MainActivity) getActivity();
         activity.layout = "HacerValoracion";
 
-        Toast.makeText(activity, "LListaSkill: " + activity.llistaSkillSelected.getNom() + ", UsuariValorat: " + activity.usuariValorat.getNom()   , Toast.LENGTH_SHORT).show();
+
+
+//        Toast.makeText(activity, "LListaSkill: " + activity.llistaSkillSelected.getNom() + ", UsuariValorat: " + activity.usuariValorat.getNom()   , Toast.LENGTH_SHORT).show();
 
         if (activity.skillSelected != null)
         {
@@ -49,12 +54,30 @@ public class ValoracionDocenteFragment extends Fragment {
 //            for (Skill skill: skills) {
 //                Toast.makeText(activity,  "Skill a Valorar: " + skill.getNom(), Toast.LENGTH_SHORT).show();
 //            }
+
+            vpSkillsAValorar = view.findViewById(R.id.ListSkillsAValorar);
+            vpSkillsAValorar.setClipToPadding(false);
+            vpSkillsAValorar.setPadding(100, 0, 100, 0);
+            vpSkillsAValorar.setPageMargin(100);
+            SkillsValoracionAdapterViewPager skillsValoracionAdapterViewPager = new SkillsValoracionAdapterViewPager(getContext(),skills,activity);
+            vpSkillsAValorar.setAdapter(skillsValoracionAdapterViewPager);
+
+
         }
         else
         {
 //            for (Skill skill: activity.llistaSkillSelected.getSkills()) {
 //                Toast.makeText(activity, skill.getNom(), Toast.LENGTH_SHORT).show();
 //            }
+
+            vpSkillsAValorar = view.findViewById(R.id.ListSkillsAValorar);
+            vpSkillsAValorar.setClipToPadding(false);
+            vpSkillsAValorar.setPadding(100, 0, 100, 0);
+            vpSkillsAValorar.setPageMargin(100);
+            SkillsValoracionAdapterViewPager skillsValoracionAdapterViewPager = new SkillsValoracionAdapterViewPager(getContext(),activity.llistaSkillSelected.getSkills(),activity);
+            vpSkillsAValorar.setAdapter(skillsValoracionAdapterViewPager);
+
+
         }
 
 
