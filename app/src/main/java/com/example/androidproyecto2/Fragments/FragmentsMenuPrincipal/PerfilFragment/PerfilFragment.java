28f7f421c2f9480c56d6f324fb964847a94cc0ee
david345672 <davidmur12@gmail.com>
2022-04-14 19:54:50 +0200

    update

--- app/src/main/java/com/example/androidproyecto2/Fragments/FragmentsMenuPrincipal/PerfilFragment/PerfilFragment.java
+++ app/src/main/java/com/example/androidproyecto2/Fragments/FragmentsMenuPrincipal/PerfilFragment/PerfilFragment.java
@@ -2,6 +2,8 @@ package com.example.androidproyecto2.Fragments.FragmentsMenuPrincipal.PerfilFrag
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -18,5 +20,13 @@ public class PerfilFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_perfil, container, false);
         return view;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
