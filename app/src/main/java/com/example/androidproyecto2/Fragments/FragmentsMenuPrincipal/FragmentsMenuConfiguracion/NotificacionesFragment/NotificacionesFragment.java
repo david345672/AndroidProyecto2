c0a55b53478ded0bcdf36f6f6137c42ee5c66de3
david345672 <davidmur12@gmail.com>
2022-04-26 12:24:07 +0200

    update

--- app/src/main/java/com/example/androidproyecto2/Fragments/FragmentsMenuPrincipal/FragmentsMenuConfiguracion/NotificacionesFragment/NotificacionesFragment.java
+++ app/src/main/java/com/example/androidproyecto2/Fragments/FragmentsMenuPrincipal/FragmentsMenuConfiguracion/NotificacionesFragment/NotificacionesFragment.java
@@ -2,6 +2,8 @@ package com.example.androidproyecto2.Fragments.FragmentsMenuPrincipal.FragmentsM
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -18,5 +20,14 @@ public class NotificacionesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_notificaciones, container, false);
         return view;
+    }
+
+
+    @Override
+    public void onViewCreated( View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
     }
 }
