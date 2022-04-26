107e6145da23a4a1f93fa1d9607e50a808ed2029
david345672 <davidmur12@gmail.com>
2022-04-26 12:29:12 +0200

    update

---
+++ app/src/main/java/com/example/androidproyecto2/Fragments/FragmentsMenuPrincipal/FragmentsMenuConfiguracion/NotificacionesFragment/CalendarMesesAdapter.java
@@ -0,0 +1,145 @@
+package com.example.androidproyecto2.Fragments.FragmentsMenuPrincipal.FragmentsMenuConfiguracion.NotificacionesFragment;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.androidproyecto2.Clases.CustomCalendar.Dia;
+import com.example.androidproyecto2.Clases.CustomCalendar.Mes;
+import com.example.androidproyecto2.Clases.Valoracio;
+import com.example.androidproyecto2.Fragments.FragmentsDocente.VerValoraciones.VerValoracionesDocenteFragment.DiasAdapter;
+import com.example.androidproyecto2.MainActivity;
+import com.example.androidproyecto2.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class CalendarMesesAdapter extends PagerAdapter
+{
+    private Context context;
+    private ArrayList<Mes> meses;
+    private MainActivity activity;
+    private List<Valoracio> valoracions;
+
+
+    public CalendarMesesAdapter(Context context, ArrayList<Mes> meses, MainActivity activity, List<Valoracio> valoracions) {
+        this.context = context;
+        this.meses = meses;
+        this.activity = activity;
+        this.valoracions = valoracions;
+    }
+
+
+    @Override
+    public int getCount() {
+        return meses.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position)
+    {
+        View view = LayoutInflater.from(context).inflate(R.layout.calendar_vpager_mes_item,container,false);
+
+        TextView lblMesAño = view.findViewById(R.id.lblMesAño);
+        Mes mes = meses.get(position);
+
+        ArrayList<Valoracio> valoracionsMes = cogerValoracionesDeMes(mes.getNum(), valoracions);
+        HashSet<Dia> diasMesValoraciones = cogerDiasMesDeValoraciones(mes.getDias(),valoracionsMes);
+
+        ArrayList<Dia> diasMes = new ArrayList<>(diasMesValoraciones);
+        Collections.sort(diasMes);
+
+
+        lblMesAño.setText(mes.getNombre() + " de " + mes.getAño());
+        RecyclerView ListDias = view.findViewById(R.id.ListDias);
+        DiasAdapter diasAdapter = new DiasAdapter(context,diasMes,valoracionsMes,activity);
+        ListDias.setHasFixedSize(true);
+        ListDias.setLayoutManager(new LinearLayoutManager(context,
+                LinearLayoutManager.VERTICAL,
+                false));
+
+        ListDias.setAdapter(diasAdapter);
+
+
+        container.addView(view);
+
+        return view;
+    }
+
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup collection, int position, Object view) {
+        collection.removeView((View) view);
+    }
+
+
+    public ArrayList<Valoracio> cogerValoracionesDeMes(int posMes, List<Valoracio> valoracions)
+    {
+
+        ArrayList<Valoracio> valoracionesDeMeses = new ArrayList<>();
+
+        for (int i = 0; i < valoracions.size(); i++)
+        {
+            char [] numFecha = valoracions.get(i).getData().toCharArray();
+            String mesStr = new StringBuilder(numFecha[4]).append(numFecha[5]).toString();
+            int mes = Integer.parseInt(mesStr);
+            //Toast.makeText(context, "mes: " + mes, Toast.LENGTH_SHORT).show();
+            if (posMes == mes)
+            {
+                valoracionesDeMeses.add(valoracions.get(i));
+            }
+
+        }
+        return valoracionesDeMeses;
+    }
+
+
+    //Coger todos los dias sin repedidos del mes donde se han hecho valoraciones de ese mes
+    /*
+     - ArrayList<Dia> dias = arrayList de dias de el mes
+     - ArrayList<Valoracio> valoracions = arrayList de valoraciones de ese mes
+     */
+    public HashSet<Dia> cogerDiasMesDeValoraciones(ArrayList<Dia> dias, List<Valoracio> valoracions)
+    {
+        HashSet<Dia> diasValoracion = new HashSet<>();
+
+        for (int i = 0; i < valoracions.size(); i++)
+        {
+            char [] numFecha = valoracions.get(i).getData().toCharArray();
+
+            char[] diasC = new char[2];
+            diasC[0] = numFecha[6];
+            diasC[1] = numFecha[7];
+            String diaStr = String.valueOf(diasC);
+            int dia = Integer.parseInt(diaStr);
+
+            for (int j = 0; j < dias.size(); j++)
+            {
+                if (dia == dias.get(j).getNum())
+                {
+                    diasValoracion.add(dias.get(j));
+                }
+            }
+
+        }
+
+
+        return diasValoracion;
+    }
+
+
+
+}
--- app/src/main/java/com/example/androidproyecto2/Fragments/FragmentsMenuPrincipal/FragmentsMenuConfiguracion/NotificacionesFragment/NotificacionesFragment.java
+++ app/src/main/java/com/example/androidproyecto2/Fragments/FragmentsMenuPrincipal/FragmentsMenuConfiguracion/NotificacionesFragment/NotificacionesFragment.java
@@ -10,7 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.androidproyecto2.Clases.CustomCalendar.Dia;
+import com.example.androidproyecto2.Clases.CustomCalendar.Mes;
 import com.example.androidproyecto2.R;
+
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class NotificacionesFragment extends Fragment {
 
@@ -27,6 +38,67 @@ public class NotificacionesFragment extends Fragment {
     public void onViewCreated( View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        
+
     }
+
+
+
+    //Coger los meses del año actual
+    public ArrayList<Mes> getMeses()
+    {
+        //coger año actual
+        Calendar cal = Calendar.getInstance();
+        int anio = cal.get(Calendar.YEAR);
+
+        //Crear arrayList de meses y coger los nombres de los meses del año en un array normal
+        ArrayList<Mes> meses = new ArrayList<Mes>();
+        String[] months = new DateFormatSymbols().getMonths();
+
+        //como siempre son 12, recorrer con un for 12 veces para llenar el arrayList de meses
+        for (int i = 0; i < months.length; i++)
+        {
+            //Los meses empiezan en 0, coger todos los dias del año actual, del mes i donde Enero sera 0, Febrero 1,...
+            Calendar mycal = new GregorianCalendar(anio, i,1);
+            int daysInMonth = mycal.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+            //Crear ArrayList de dias donde guardaremos el numero y el nombre, Ej: 2 y Lunes
+            ArrayList<Dia> dias = new ArrayList<>();
+
+            //recorremos cada uno de los dias iniciando en 1 para tener el dia exacto
+            for (int d = 1; d <= daysInMonth; d++)
+            {
+                //Crear un String con la fecha del año actual, el mes que en este caso no empieza en 0 sino en 1, y el dia
+                String dateString = String.format("%d-%d-%d", anio, i + 1, d);
+                Date date = null;
+                try {
+                    //Creamos un objeto Date con los datos del dateString y cogemos el nombre del dia de la semana
+                    //Le ponemos el idioma que queremos en mi caso pongo Español
+                    date = new SimpleDateFormat("yyyy-M-d").parse(dateString);
+                    String dayOfWeek = new SimpleDateFormat("EEEE", new Locale("es", "ES")).format(date);
+
+
+                    //Una vez cogido el dia y el nombre, crear un objeto Dia para añadir lo al array de dias
+                    Dia dia = new Dia(d,dayOfWeek);
+                    dias.add(dia);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            //Despues de llenar el arrayList de dias, Crear un objeto Mes con el año actual, el nombre del mes que en nuestro caso sera
+            //la iteracion del array de meses, el numero del mes, Ej Enero es 1, Abril es 4,... y el arrayList de dias, Finalmente añadimos el mes en el arrayList de meses
+            Mes mes = new Mes(anio,months[i],i + 1,dias);
+
+            meses.add(mes);
+
+        }
+
+
+        return meses;
+    }
+
+
+
+
+
 }
